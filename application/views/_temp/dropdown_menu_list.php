010cc86ffec6efe133907158ac92a9a6b176de0f
Daryl V <darylv@DV-MBP.local>
2015-04-24 16:36:03 +0800

    add controller for home.php, addemployee.php, uploadcsv.php, for linking purposes

--- application/views/_temp/dropdown_menu_list.php
+++ application/views/_temp/dropdown_menu_list.php
@@ -1,6 +1,7 @@
  
-  <li><a href="#" data-toggle="modal" data-target="#gsx-Modal">Upload CSV file</a></li>
-  <li><a href="#" data-toggle="modal" data-target="#email-Modal" >Add Employees</a></li>
+  <li><a href="#" class="home" data-toggle="modal" data-target="#gsx-Modal">Home</a></li>
+  <li><a href="#" class="upload-csv" data-toggle="modal" data-target="#gsx-Modal">Upload CSV file</a></li>
+  <li><a href="#" class="add-employee" data-toggle="modal" data-target="#email-Modal" >Add Employees</a></li>
   <li><a href="#" class="add-user-account" data-toggle="modal" data-target="#gsx-Modal">Add User Accounts</a></li>
   <li class="btn-config-set-menu-divider divider"></li>
-  <li><a href="login.php">Sign Out</a></li>
+  <li><a class="logouttk" href="login.php">Sign Out</a></li>
